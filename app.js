36d0c28bc8b5df27c5bf0abf3c8936d81dc0e68a
Leon Lamle <llamle@gmail.com>
2015-09-23 14:42:38 -0400

    testing js folder path

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 'use strict';
 
+function openFolderDialog(){
+  var inputField = document.querySelector('#folderSelector');
+  inputField.addEventListener('change', function(){
+    var folderPath = this.value;
+    alert('Folder path is ' + folderPath);
+  }, false);
+  inputField.click();
+}
+
 function bindSelectFolderClick(){
   var button = document.querySelector('#select_folder');
   button.addEventListener('click', function(){
-    alert('clicked on the button');
+    openFolderDialog();
   });
 }
 
